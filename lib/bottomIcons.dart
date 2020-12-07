6cb97e4adea2b4f98f592c6e159515e02c11fc37
Orion028 <sumansasmal028@gmail.com>
2020-12-07 22:26:17 +0530

    Popup window inserted

--- lib/bottomIcons.dart
+++ lib/bottomIcons.dart
@@ -11,7 +11,7 @@ class BottomIcons extends StatelessWidget {
           margin: EdgeInsets.only(left: 20),
           child: IconButton(
             alignment: Alignment.centerLeft,
-            icon: Icon(Icons.menu_rounded, color: Colors.white, size: 30),
+            icon: Icon(Icons.info_outline_rounded, color: Colors.white, size: 30),
             onPressed: () => (() {
               //somthing need to be written
             }),
@@ -23,12 +23,37 @@ class BottomIcons extends StatelessWidget {
           child: IconButton(
             alignment: Alignment.centerRight,
             icon: Icon(Icons.more_vert, color: Colors.white, size: 30),
-            onPressed: () => (() {
-              //somthing need to be written
+            onPressed: () {
+             _popupModalBottomSheet(context);
             }),
           ),
-        ),
       ],
     );
   }
+  //POPUP
+  void _popupModalBottomSheet(context) {
+    showModalBottomSheet(context: context, builder: (BuildContext bc){
+      return Container(
+        height: MediaQuery.of(context).size.height * .50,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text("Authors"),
+                  Spacer(),
+                  IconButton(
+                    icon: Icon(Icons.cancel, color: Colors.black, size: 25, ), 
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    })
+                ],
+              ),
+            ],
+          ),
+        )
+      );
+    });
+    }
 }
