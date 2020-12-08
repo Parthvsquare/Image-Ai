b892ea716f583bf019402902a6702e233dc80b8c
Orion028 <sumansasmal028@gmail.com>
2020-12-08 21:03:49 +0530

    Popup done yeah!!!!!!!!!!!

--- lib/bottomIcons.dart
+++ lib/bottomIcons.dart
@@ -1,4 +1,8 @@
+// ignore: avoid_web_libraries_in_flutter
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:test1/constant.dart';
 
 class BottomIcons extends StatelessWidget {
   @override
@@ -34,22 +38,99 @@ class BottomIcons extends StatelessWidget {
   void _popupModalBottomSheet(context) {
     showModalBottomSheet(context: context, builder: (BuildContext bc){
       return Container(
-        height: MediaQuery.of(context).size.height * .50,
+        height: MediaQuery.of(context).size.height * .30,
+        color: PrimaryColor,
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
           child: Column(
             children: <Widget>[
               Row(
                 children: <Widget>[
-                  Text("Authors"),
+                  Text('Authors',
+                  style: TextStyle(
+                  fontSize: 32,
+                  fontFamily: 'OpenSans',
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,),
+                  ),
                   Spacer(),
                   IconButton(
-                    icon: Icon(Icons.cancel, color: Colors.black, size: 25, ), 
+                    icon: Icon(Icons.cancel, color: Colors.white, size: 25, ), 
                     onPressed: (){
                       Navigator.of(context).pop();
                     })
                 ],
               ),
+              Divider(
+                height: 10,
+                color: Colors.white,
+              ),
+              Row(
+                children: <Widget>[
+                  Text('Parth Varanase',
+                  style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: 'OpenSans',
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,),
+                  ),
+                  Spacer(),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+	                  shape: BoxShape.circle,
+	                  image: DecorationImage(
+                      image: AssetImage('assets/Parth.png'),
+	                  fit: BoxFit.fill
+                	  ),
+                  ),
+                  ),
+                ],),
+              Row(
+                children: <Widget>[
+                  Text('Dhiren Boricha',
+                  style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.w400,),
+                  ),
+                   Spacer(),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+	                  shape: BoxShape.circle,
+	                  image: DecorationImage(
+                      image: AssetImage('assets/Dhiren.png'),
+	                  fit: BoxFit.fill
+                	  ),
+                  ),
+                  ),
+                ],),
+               Row(
+                children: <Widget>[
+                  Text('Suman Sasmal',
+                  style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.w400,),
+                  ),
+                   Spacer(),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+	                  shape: BoxShape.circle,
+	                  image: DecorationImage(
+                      image: AssetImage('assets/Suman.png'),
+	                  fit: BoxFit.fill
+                	  ),
+                  ),
+                  ),
+                ],),    
             ],
           ),
         )
